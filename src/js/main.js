701f18e3c86cd15bf3e79f0325d0b6f965048eae
sonangrai <shonahangrae@gmail.com>
2021-09-18 10:43:36 +0545

    added tagify

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,4 @@
 
 (function () {
   console.log("Have a good day 🎈");
-
-  /**
-   * For label like of the matareial
-   */
-  let mInputs = document.querySelectorAll(".minput");
-  mInputs.forEach((i) => {
-    i.addEventListener("change", () => {
-      if (i.value.length !== 0) {
-        i.classList.add("mshow");
-      } else {
-        i.classList.remove("mshow");
-      }
-    });
-  });
 })();
